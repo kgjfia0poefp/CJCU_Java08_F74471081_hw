551d4f72d2a04b49115042eea8b55805464a51dd
kgjfia0poefp <92013723+kgjfia0poefp@users.noreply.github.com>
2021-11-06 13:03:01 +0800

    Add files via upload

---
+++ Class13.java
@@ -0,0 +1,28 @@
+package Java08;
+class CCircle
+{
+	double area(double r)
+	{
+		return (3.14*r*r);
+	}
+	float area(float r)
+	{
+		return (3.14f*r*r);
+	}
+	double area(int r)
+	{
+		return (3.14*r*r);
+	}
+}
+public class Class13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CCircle obj=new CCircle();
+		
+		System.out.println("area(2)="+obj.area(2));
+		System.out.println("area(2.2f)="+obj.area(2.2f));
+		System.out.println("area(2.2)="+obj.area(2.2));
+	}
+
+}
